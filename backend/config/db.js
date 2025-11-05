5aaa62d480dff4d36103e87ab1f4501ad0f1cba7
RonShlomo97 <109730205+RonShlomo97@users.noreply.github.com>
2025-11-06 01:10:36 +0200

    db render

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,13 +4,23 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-});
+const isRender = !!process.env.RENDER;
+
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        user: process.env.PGUSER,
+        host: process.env.PGHOST,
+        database: process.env.PGDATABASE,
+        password: process.env.PGPASSWORD,
+        port: process.env.PGPORT,
+        ssl: false,
+      }
+);
 
 pool
   .connect()
